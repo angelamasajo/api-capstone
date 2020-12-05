1ce7501a655bb5fb66113d8dd9d0a6b8e71f27dc
Angela Masajo <aabmasajo@gmail.com>
2020-12-05 17:00:14 -0600

    weather info update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//----------------------------WEATHER INFO--------------------------/
+
 
 // FETCHING WEATHER INFORMATION
 function getWeatherInfo (cityNameWeather) {
@@ -18,7 +20,6 @@ function getWeatherInfo (cityNameWeather) {
         .catch(error => alert ('Something went wrong.'))
 }
 
-// http://api.openweathermap.org/data/2.5/forecast?q=milwaukee&appid=1797367c5f35ceaf3262c0d376362893&units=imperial
 
 //DISPLAYING WEATHER INFO
 function displayWeatherResults(responseJson) {
@@ -38,30 +39,25 @@ function displayWeatherResults(responseJson) {
     $('#weather-results').removeClass('hidden');
 };
 
-// function pickDay () {
-//     $('#weather-results').on('submit', event => {
-//         event.preventDefault();
-//         console.log('pickDay ran');
-//         //clears out the list
-//         //assigning variables; goal is to include name and address of the brewery in the final message
-//         let date = $(event.target).closest('#pick-day-form').find('#inputWeather').text();
-//         console.log(date);
-//         let weatherDescription = $(event.target).closest('#pick-day-form').find('#weather-description').text();
-//         console.log(weatherDescription);
-//         // let cityWeather = $(event.target).closest('#pick-day-form').find('#brewery-city').text();
-//         // console.log(cityWeather);
-//         $('#weather-results').empty();
-//         // //adding the brewery name and address in the final message to copy and paste
-//         $('.message').removeClass('hidden');
-//         // // let div = document.getElementById('#final-message');
-//         // // let content = document.createTextNode(`${title}, ${address}`);
-//         // // div.appendChild(content);
-//         showInviteMessage(title, address, cityWeather, date)
-//     })
-// }
+function pickDay () {
+    $('#weather-results').on('submit', event => {
+        event.preventDefault();
+        console.log('pickDay ran');
+        //clears out the list
+        //assigning variables; goal is to include name and address of the brewery in the final message
+        let date = $(event.target).closest('#pick-day-form').find('#inputWeather').text();
+        console.log(date);
+        let weatherDescription = $(event.target).closest('#pick-day-form').find('#weather-description').text();
+        console.log(weatherDescription);
+        $('#weather-results').empty();
+        // //adding the brewery name and address in the final message to copy and paste
+        $('.message').removeClass('hidden');
+        showInviteMessage(title, address, cityBrewery, date)
+    })
+}
 
 
-
+//------------------------BREWERY INFO----------------------------//
 
 //FETCHING BREWERY INFORMATION
 function getBreweryInfo (cityName) {
@@ -108,27 +104,28 @@ function pickBrewery () {
         console.log(title);
         let address = $(event.target).closest('#pick-brewery-form').find('#brewery-address').text();
         console.log(address);
-        let cityWeather = $(event.target).closest('#pick-brewery-form').find('#brewery-city').text();
-        console.log(cityWeather);
+        let cityBrewery = $(event.target).closest('#pick-brewery-form').find('#brewery-city').text();
+        console.log(cityBrewery);
         $('#brewery-results').empty();
         //adding the brewery name and address in the final message to copy and paste
         $('.message').removeClass('hidden');
         // let div = document.getElementById('#final-message');
         // let content = document.createTextNode(`${title}, ${address}`);
         // div.appendChild(content);
-        showInviteMessage(title, address, cityWeather)
+        showInviteMessage(title, address, cityBrewery)
     })
 }
 
 
 
-function showInviteMessage (title, address, cityWeather) {
+function showInviteMessage (title, address, cityBrewery) {
     $('.message').html(
-            `<p>
+            `<h3>
             Now that you've chosen your time and location, copy and paste the information below and send to your friends!
-
+            </h3>
+            <p>
             Hello friends! I think it's time for all of us to catch up! I would love it if you can join me at ${title} located 
-            in ${address}, ${cityWeather}. The weather is supposed to be nicest on (date), so it's probably a good idea to go then! Please 
+            in ${address}, ${cityBrewery}. The weather is supposed to be nicest on (date), so it's probably a good idea to go then! Please 
             respond to this message if you can make it so we can get more details figured out. Hope you can make it!
             </p>
             `
@@ -158,4 +155,4 @@ function onWeatherSubmit (cityNameWeather) {
 $(onWeatherSubmit)
 $(watchBrewerySearchForm);
 $(pickBrewery);
-// $(pickDay);
+$(pickDay);
