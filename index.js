76df07e8f3a2a1f55ae17965a98069c1bb68031a
Angela Masajo <aabmasajo@gmail.com>
2020-12-05 20:09:48 -0600

    date and brewery populating msg template

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //----------------------------WEATHER INFO--------------------------/
-
+let current_obj = {};
 
 
 
@@ -42,6 +42,8 @@ function displayWeatherResults(responseJson) {
     $('#weather-results').removeClass('hidden');
 };
 
+
+
 function pickDay () {
     $('#weather-results').on('submit', event => {
         event.preventDefault();
@@ -49,15 +51,14 @@ function pickDay () {
         //clears out the list
         //assigning variables; goal is to include name and address of the brewery in the final message
         let date = $(event.target).closest('#pick-day-form').find('#inputWeather').text();
+        current_obj.date = date;
         console.log(date);
         let weatherDescription = $(event.target).closest('#pick-day-form').find('#weather-description').text();
         console.log(weatherDescription);
         $('#weather-results').empty();
-        // //adding the brewery name and address in the final message to copy and paste
-        $('.message').removeClass('hidden');
-        showInviteMessage(title, address, cityBrewery, date)
     })
 }
+
 
 
 //------------------------BREWERY INFO----------------------------//
@@ -104,36 +105,50 @@ function pickBrewery () {
         //clears out the list
         //assigning variables; goal is to include name and address of the brewery in the final message
         let title = $(event.target).closest('#pick-brewery-form').find('#input').text();
+        current_obj.title = title
         console.log(title);
         let address = $(event.target).closest('#pick-brewery-form').find('#brewery-address').text();
+        current_obj.address = address
         console.log(address);
         let cityBrewery = $(event.target).closest('#pick-brewery-form').find('#brewery-city').text();
+        current_obj.cityBrewery = cityBrewery
         console.log(cityBrewery);
         $('#brewery-results').empty();
         //adding the brewery name and address in the final message to copy and paste
         $('.message').removeClass('hidden');
-        // let div = document.getElementById('#final-message');
-        // let content = document.createTextNode(`${title}, ${address}`);
-        // div.appendChild(content);
-        showInviteMessage(title, address, cityBrewery)
+        displayData(current_obj);
     })
 }
 
-
-
-function showInviteMessage (title, address, cityBrewery) {
-    $('.message').html(
-            `<h3>
-            Now that you've chosen your time and location, copy and paste the information below and send to your friends!
-            </h3>
-            <p>
-            Hello friends! I think it's time for all of us to catch up! I would love it if you can join me at ${title} located 
-            in ${address}, ${cityBrewery}. The weather is supposed to be nicest on (date), so it's probably a good idea to go then! Please 
-            respond to this message if you can make it so we can get more details figured out. Hope you can make it!
-            </p>
-            `
-     )
+function displayData(current_obj) { 
+    $('.message').append(
+        `<h3> Now that you've chosen your time and location, copy and paste 
+        the information below and send to your friends!
+        </h3>
+        <p> Hello friends! I think it's time for all of us to catch up! 
+        I would love it if you can join me at ${current_obj.title} 
+        located in ${current_obj.address}, ${current_obj.cityBrewery}. 
+        The weather is supposed to be nicest on ${current_obj.date}, 
+        so it's probably a good idea to go then! 
+        Please respond to this message if you can make it so we can 
+        get more details figured out. Hope you can make it! </p>` 
+    )
 }
+
+
+// function showInviteMessage (title, address, cityBrewery) {
+//     $('.message').html(
+//             `<h3>
+//             Now that you've chosen your time and location, copy and paste the information below and send to your friends!
+//             </h3>
+//             <p>
+//             Hello friends! I think it's time for all of us to catch up! I would love it if you can join me at ${title} located 
+//             in ${address}, ${cityBrewery}. The weather is supposed to be nicest on (date), so it's probably a good idea to go then! Please 
+//             respond to this message if you can make it so we can get more details figured out. Hope you can make it!
+//             </p>
+//             `
+//      )
+// }
 
 
 
